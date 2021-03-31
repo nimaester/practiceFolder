e7c1dcc5d1b55ae21fb70b7c1e6b29bce81c6946
nimaester <nimaester@gmail.com>
2021-03-30 19:54:40 -0700

    did frequecy counter

---
+++ frequencyCounter.js
@@ -0,0 +1,32 @@
+const countFreq = (ele) => {
+  const freq = {};
+  for (let char of String(ele)) {
+    freq[char] ? freq[char]++ : (freq[char] = 1);
+  }
+  return freq;
+};
+
+console.log(countFreq(132456));
+
+function sameFrequency(num1, num2) {
+  // good luck. Add any arguments you deem necessary.
+  if (String(num1).length !== String(num2).length) {
+    return false;
+  }
+
+  let freq1 = countFreq(num1);
+  let freq2 = countFreq(num2);
+
+  for (let key in freq1) {
+    if (freq1[key] !== freq2[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(sameFrequency(182, 812));
+console.log(sameFrequency(18222, 82212));
+console.log(sameFrequency(11182, 812));
+console.log(sameFrequency(18821, 88121));
+console.log(sameFrequency(1834432, 8134332));
