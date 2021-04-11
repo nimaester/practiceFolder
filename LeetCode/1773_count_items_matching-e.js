f06b634e1d8c8e0427b1026635e8294eeba1edda
nimaester <nimaester@gmail.com>
2021-04-11 03:52:53 -0700

    solved counting matching items

---
+++ LeetCode/1773_count_items_matching-e.js
@@ -0,0 +1,48 @@
+// 1773. Count Items Matching a Rule
+
+// You are given an array items, where each items[i] = [typei, colori, namei] describes the type, color, and name of the ith item. You are also given a rule represented by two strings, ruleKey and ruleValue.
+
+// The ith item is said to match the rule if one of the following is true:
+
+// ruleKey == "type" and ruleValue == typei.
+// ruleKey == "color" and ruleValue == colori.
+// ruleKey == "name" and ruleValue == namei.
+// Return the number of items that match the given rule.
+
+// Example 1:
+
+// Input: items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], ruleKey = "color", ruleValue = "silver"
+// Output: 1
+// Explanation: There is only one item matching the given rule, which is ["computer","silver","lenovo"].
+
+// Example 2:
+
+// Input: items = [["phone","blue","pixel"],["computer","silver","phone"],["phone","gold","iphone"]], ruleKey = "type", ruleValue = "phone"
+// Output: 2
+// Explanation: There are only two items matching the given rule, which are ["phone","blue","pixel"] and ["phone","gold","iphone"]. Note that the item ["computer","silver","phone"] does not match.
+
+// @param {string[][]} items
+// @param {string} ruleKey
+// @param {string} ruleValue
+// @return {number}
+
+var countMatches = function (items, ruleKey, ruleValue) {
+  let values = ["type", "color", "name"];
+  let key = values.indexOf(ruleKey);
+  let count = 0;
+  for (let i = 0; i < items.length; i++) {
+    let item = items[i];
+    if (item[key] === ruleValue) {
+      count++;
+    }
+  }
+  return count;
+};
+
+let items2 = [
+  ["phone", "blue", "pixel"],
+  ["computer", "silver", "phone"],
+  ["phone", "gold", "iphone"],
+];
+// ruleKey = "type", ruleValue = "phone"
+console.log(countMatches(items2, "type", "phone"));
