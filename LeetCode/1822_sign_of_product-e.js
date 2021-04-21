060325fa8840d60f387b700fc2dfb05edae9eeaf
nimaester <nimaester@gmail.com>
2021-04-20 21:43:34 -0700

    solved 1822

---
+++ LeetCode/1822_sign_of_product-e.js
@@ -0,0 +1,28 @@
+// 1822. Sign of the Product of an Array
+
+// There is a function signFunc(x) that returns:
+
+// 1 if x is positive.
+// -1 if x is negative.
+// 0 if x is equal to 0.
+// You are given an integer array nums. Let product be the product of all values in the array nums.
+
+// Return signFunc(product).
+
+// @param {number[]} nums
+// @return {number}
+
+var arraySign = function (nums) {
+  if (nums.includes(0)) {
+    return 0;
+  }
+
+  let numberOfPos = nums.filter((num) => num < 0);
+  if (numberOfPos.length % 2 === 0) {
+    return 1;
+  } else {
+    return -1;
+  }
+};
+
+console.log(arraySign([-1, -2, -3, -4, 3, 2, 1]));
