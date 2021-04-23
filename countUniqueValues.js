0e55cc7c22ea8c77e10ef533ad9879c26d1fab32
nimaester <nimaester@gmail.com>
2021-04-23 12:23:39 -0700

    countUniqueValues

--- countUniqueValues.js
+++ countUniqueValues.js
@@ -1,7 +1,7 @@
 // Implement a function called countUniqueValues which accepts a sorted
 // array and counts the unique countUniqueValuesin the array. There can
 // be negative numbers in the array but will always be sorted
-// use the sliding window pattern
+// use the multiple pointers pattern
 // Effecient because we only go through the array once
 
 // [1,1,1,2,3,3,4,4]
