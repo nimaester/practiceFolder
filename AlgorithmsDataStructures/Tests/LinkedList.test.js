5bc0e3837faea3e25337dd5776f352375a8a31a2
nimaester <nimaester@gmail.com>
2021-05-03 15:10:54 -0700

    added more tests

--- AlgorithmsDataStructures/Tests/LinkedList.test.js
+++ AlgorithmsDataStructures/Tests/LinkedList.test.js
@@ -48,19 +48,85 @@ describe("#removeHead", () => {
 
 describe("#getAtIndex", () => {
   test("return the value in a given index", () => {
-    const ll = LinkedList.addToList("zero", "one", "two", "three");
+    const ll = LinkedList.createLinkedList("three", "two", "one", "zero");
 
-    expect(ll.getAtIndex(0)).toBe("zero");
-    expect(ll.getAtIndex(3)).toBe("three");
-    expect(ll.getAtIndex(1)).toBe("one");
-    expect(ll.getAtIndex(-3)).toBe(null);
-    expect(ll.getAtIndex(15)).toBe(null);
+    expect(ll.getAtIndex(0).value).toBe("three");
+    expect(ll.getAtIndex(3).value).toBe("zero");
+    expect(ll.getAtIndex(1).value).toBe("two");
+  });
+
+  describe("with an index greater than Linked List length", () => {
+    test("returns null", () => {
+      const ll = LinkedList.createLinkedList("three", "two", "one", "zero");
+      expect(ll.getAtIndex(15)).toBeNull();
+      expect(ll.getAtIndex(10)).toBeNull();
+    });
+  });
+
+  describe("with an index less than zero", () => {
+    test("returns null", () => {
+      const ll = LinkedList.createLinkedList("three", "two", "one", "zero");
+      expect(ll.getAtIndex(-1)).toBeNull();
+      expect(ll.getAtIndex(-22)).toBeNull();
+    });
   });
 });
 
-describe("#displayValues", () => {
-  test("list the values in the Linked List in an array", () => {
-    const ll = LinkedList.addToList("zero", "one", "two", "three");
-    expect(ll.displayValues()).toStrictEqual(["zero", "one", "two", "three"]);
+describe("#print", () => {
+  test("list the values in the Linked List", () => {
+    const ll = new LinkedList();
+    ll.addToHead(3);
+    ll.addToHead(2);
+    ll.addToHead(1);
+
+    expect(ll.print()).toBe(console.log("1 > 2 > 3 > null"));
+  });
+});
+
+describe("#insertAtIndex", () => {
+  test("returns a new Linked List with a value inserted at index", () => {
+    const ll = LinkedList.createLinkedList("three", "two", "one", "zero");
+    ll.insertAtIndex(1, "insertedValue");
+    expect(ll.getAtIndex(1).value).toBe("insertedValue");
+  });
+
+  describe("insert at 0 index", () => {
+    test("returns a new Linked List with a value inserted at index 0", () => {
+      const ll = LinkedList.createLinkedList("three", "two", "one", "zero");
+      ll.insertAtIndex(0, "insertedValue");
+      expect(ll.getAtIndex(0).value).toBe("insertedValue");
+    });
+  });
+
+  describe("insert at index higher than the length of the Linked List", () => {
+    test("returns null", () => {
+      const ll = LinkedList.createLinkedList("three", "two", "one", "zero");
+      ll.insertAtIndex(15, "insertedValue");
+      expect(ll.getAtIndex(15)).toBeNull();
+    });
+  });
+
+  describe("insert at index lower than 0", () => {
+    test("returns null", () => {
+      const ll = LinkedList.createLinkedList("three", "two", "one", "zero");
+      ll.insertAtIndex(-11, "insertedValue");
+      expect(ll.getAtIndex(-11)).toBeNull();
+    });
+  });
+});
+
+describe("#removeAtIndex", () => {
+  test("removes from a given index", () => {
+    const ll = LinkedList.createLinkedList(1, 2, 3, 4);
+    ll.removeAtIndex(1);
+    ll.removeAtIndex(1);
+    expect(ll.print()).toBe(console.log("1 > 4 > null"));
+  });
+
+  describe("remove at an index higher than the Linked List length", () => {
+    test("returns null", () => {
+      const ll = LinkedList.createLinkedList(1, 2, 3, 4);
+      expect(ll.removeAtIndex(15)).toBeNull();
+    });
   });
 });
