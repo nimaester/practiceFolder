b21e6287b82ff172d53b0f1c749edb06918aa75c
Nim Tayo <59195400+nimaester@users.noreply.github.com>
2021-07-04 20:14:46 -0700

    Create 14_longest_common_prefix-e.js

---
+++ LeetCode/14_longest_common_prefix-e.js
@@ -0,0 +1,15 @@
+// 14. Longest Common Prefix
+
+// Share
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+
+ /**
+ * @param {string[]} strs
+ * @return {string}
+ */
+ 
+var longestCommonPrefix = function(strs) {
+    
+};
