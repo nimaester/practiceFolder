dc1827d4124c70dc9f6f6f294e3bc6bccc5aff18
Nim Tayo <59195400+nimaester@users.noreply.github.com>
2021-07-28 22:37:22 -0700

    Update checkDuplicates.js

--- checkDuplicates.js
+++ checkDuplicates.js
@@ -1,3 +1,10 @@
-const areThereDuplicates = (...args) => {};
+const areThereDuplicates = (...args) => {
+let freqs = {};
+  let solution = false;
+
+  for (let ele of args) {
+    freqs[ele] ? (solution = true) : (freqs[ele] = 1);
+  }
+  return solution;};
 
 console.log(areThereDuplicates(1, 2, 3, 1));
