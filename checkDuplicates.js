4d7212f949591456c6c8c61e88c9113498f1a8cc
Nim Tayo <nimaester@gmail.com>
2021-07-28 22:48:41 -0700

    upload

--- checkDuplicates.js
+++ checkDuplicates.js
@@ -1,11 +1,3 @@
-const areThereDuplicates = (...args) => {
-  let freqs = {};
-  let solution = false;
-
-  for (let ele of args) {
-    freqs[ele] ? (solution = true) : (freqs[ele] = 1);
-  }
-  return solution;
-};
+const areThereDuplicates = (...args) => {};
 
 console.log(areThereDuplicates(1, 2, 3, 1));
