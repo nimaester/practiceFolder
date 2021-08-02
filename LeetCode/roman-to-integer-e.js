898b6c7ea10d53533eb671c67fba7ebd8c665b5c
Nim Tayo <nimaester@gmail.com>
2021-08-02 15:51:02 -0700

    redid roman values

--- LeetCode/roman-to-integer-e.js
+++ LeetCode/roman-to-integer-e.js
@@ -47,23 +47,24 @@
  */
 
 var romanToInt = function (s) {
-  let values = [];
   let total = 0;
+  const romanValues = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
 
-  const romanNumbers = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
-
-  for (let value of s) {
-    values.push(romanNumbers[value]);
-  }
-
-  for (let i = 0; i < values.length; i++) {
-    if (values[i] < values[i + 1]) {
-      total -= values[i];
+  for (let i = 0; i < s.length; i++) {
+    if (romanValues[s[i]] < romanValues[s[i + 1]]) {
+      total -= romanValues[s[i]];
     } else {
-      total += values[i];
+      total += romanValues[s[i]];
     }
   }
-
   return total;
 };
 //
