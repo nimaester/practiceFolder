d1fd401a675633acc7579ca80916174f7d4d2c9d
Nim Tayo <nimaester@gmail.com>
2021-08-04 16:11:42 -0700

    solved merge 2 sorted lists

--- LeetCode/21_merge_two_sorted_lists-e.js
+++ LeetCode/21_merge_two_sorted_lists-e.js
@@ -12,13 +12,15 @@
  */
 var mergeTwoLists = function (l1, l2) {
   // if one list is emtpy return the other one
-  // else we we would declare an variable to hold values of our sorted list XX
+  // else we we would declare an variable to hold values of our sorted list
+  // arg are mandatory so set val to something and next to null
   // while there are values for each linked list we check each one of them
   // if l1 value is < l2 value, we set newList.val to l1 or l2l value
   // we need a pointer to keep track of our current position in the new List
   // then if we place the l1 or l2 value in the new List and we keep track of our position by setting l1 or l2 to their next values
   // if we reach null our while loop breaks
-  // return new linked List
+  // if l1 or l2 are not null set them to the new list next
+  // return list.next because our initial list node declaration has a value that we dont need
 
   if (!l1) {
     return l2;
@@ -26,25 +28,21 @@ var mergeTwoLists = function (l1, l2) {
     return l1;
   }
 
-  let newLl = new ListNode(0, null);
-  let pos = newLl;
+  let newNode = new ListNode(0, null);
+  let position = newNode;
 
   while (l1 && l2) {
     if (l1.val < l2.val) {
-      pos.next = l1;
+      position.next = l1;
       l1 = l1.next;
+      position = position.next;
     } else {
-      pos.next = l2;
+      position.next = l2;
       l2 = l2.next;
+      position = position.next;
     }
-    pos = pos.next;
   }
 
-  if (l1 === null) {
-    pos.next = l2;
-  } else {
-    pos.next = l1;
-  }
-
-  return newLl.next; // we return next because our initial val is 0
+  position.next = l1 || l2;
+  return newNode.next;
 };
