75827556783ad3192424e69f2a4d15950bd3b57f
Nim Tayo <nimaester@gmail.com>
2021-11-02 23:55:52 -0700

    symmetric tree

--- practice.js
+++ practice.js
@@ -1,38 +1,26 @@
-// const runningSum = (arr) => {
-//   let x = 1;
-//   let sum = x;
-//   for (let i = 0; i < arr.length; i++) {
-//     sum = sum + arr[i];
-//     console.log(`1:   sum:${sum} x:${x} i:${i}`);
-//     if (sum < 1) {
-//       while (sum < 1) {
-//         x++;
-//         sum++;
-//       }
-//     } else {
-//       sum += arr[i];
-//     }
-//     console.log(`sum:${sum} x:${x} i:${i}`);
-//   }
-//   return x;
-// };
+x = { foo: "bar" };
+y = { baz: x };
+z = y["baz"]["foo"];
 
-const runningSum = (arr) => {
-  let x = 0;
-  for (let i = x; x < Infinity; x++) {
-    let sum = x;
+let store = { a: 1 };
+let dispatch = { b: 2 };
 
-    for (let num of arr) {
-      sum += num;
-      console.log(`sum:${sum} x:${x}`);
-      if (sum < 1) {
-        break;
-      }
-    }
-    if (sum >= 1) {
-      return x;
-    }
-  }
-  return 0;
+let c = { ...store, dispatch };
+var fun = function () {
+  return {
+    log: function () {
+      console.log(this.val);
+    },
+  };
 };
-console.log(runningSum([-5, 4, -2, 3, 1, -1, -6, -1, 0, -5]));
+
+var fat = function () {
+  return {
+    log: () => {
+      console.log(this.val);
+    },
+  };
+};
+
+console.log(fat().log());
+console.log(fun());
---
+++ symmetricTree.js
@@ -0,0 +1,15 @@
+Given a root of a binary tree, check where it is a mirror of itself (i.e., symmetrice around its center)
+
+const checker = (leftNode, rightNode) => {
+  if(!leftNode && !rightNode) return true;
+  if (!leftNode || !rightNode) return false;
+  if(leftNode.val !== rightNode.val) return false;
+
+  return checker(leftNode.left, rightNode.right) && checker(leftNode.left, rightNode.right)
+}
+
+const isSymmetricTree = (root) => {
+  if (!root) return true;
+  return checker(root.left, root.right)
+}
+
