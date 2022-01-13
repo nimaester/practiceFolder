93303bdbb86db0cf013900cb6b75f5a5ceaff5ae
Nim Tayo <nimaester@gmail.com>
2022-01-12 18:19:44 -0800

    uniqueValues

--- recap/multiplePointers.js
+++ recap/multiplePointers.js
@@ -7,12 +7,10 @@ const sumZero = (arr) => {
   let j = arr.length - 1;
 
   while (i < j) {
-    // 2 < 2
-    if (arr[i] + arr[j] === 0) {
-      // 0 + 1 == 1
+    let sum = arr[i] + arr[j];
+    if (sum === 0) {
       return [arr[i], arr[j]];
-    } else if (arr[i] + arr[j] > 0) {
-      // 1 > 0
+    } else if (sum > 0) {
       j--;
     } else {
       i++;
@@ -25,3 +23,23 @@ console.log(sumZero([-3, -2, 0, 1, 2, 3]));
 
 // best case scenario 0(1)
 // worst case is 0(n) n = number of elements in the array
+
+const uniqueValues = (arr) => {
+  let i = 0;
+  let uniques = 0;
+  if (arr.length === 0) return uniques;
+  if (arr.length === 1) return uniques + 1;
+  for (let j = 1; j < arr.length; j++) {
+    let num1 = arr[i];
+    let num2 = arr[j];
+    if (num1 !== num2) {
+      uniques++;
+      i = j;
+    }
+  }
+  return uniques + 1; // accounting the first number that is unique
+};
+
+console.log(uniqueValues([1, 2, 2, 2, 3, 3, 4, 5, 5, 5, 6]));
+
+// best case scenario o(n) n = number of items in array
