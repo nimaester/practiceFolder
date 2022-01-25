1fc68d6b1cd0875182ab70557742651ca65f93a2
Nim Tayo <nimaester@gmail.com>
2022-01-25 00:59:14 -0800

    get & set

--- recap/singlyLinkedList.js
+++ recap/singlyLinkedList.js
@@ -27,20 +27,51 @@ class SinglyLinkedList {
   }
 
   pop() {
-    if (!this.length) return false;
-    if (this.length === 1) {
-      this.head = null;
-      this.tail = null;
-    } else {
-      let currentNode = this.head;
-      while (currentNode.next !== this.tail) {
-        currentNode = currentNode.next;
-      }
-      currentNode.next = null;
-      this.tail = currentNode;
+    if (!this.length) return undefined;
+    let currentNode = this.head;
+    let currentTail = currentNode;
+    while (currentNode.next) {
+      currentTail = currentNode;
+      currentNode = currentNode.next;
     }
-
+    this.tail = currentTail;
+    this.tail.next = null;
     this.length--;
+    if (!this.length) (this.head = null), (this.tail = null);
+  }
+
+  unshift(val) {
+    const node = new Node(val);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+    this.length++;
+  }
+
+  shift() {
+    if (!this.length) return undefined;
+    let node = this.head;
+    this.head = node.next;
+    this.length--;
+    if (this.length === 0) this.tail = null;
+  }
+
+  get(idx) {
+    if (idx > this.length - 1 || idx < 0) return null;
+    let currentNode = this.head;
+    for (let i = 0; i < idx; i++) {
+      currentNode = currentNode.next;
+    }
+    return currentNode;
+  }
+
+  set(idx, val) {
+    let node = this.get(idx);
+    return node ? ((node.val = val), true) : false;
   }
 }
 
@@ -52,4 +83,14 @@ ll.pop();
 ll.pop();
 ll.pop();
 ll.pop();
+ll.unshift("1st");
+ll.unshift("2nd");
+ll.unshift("3rd");
+
+// ll.shift();
+// ll.shift();
+// ll.shift();
+
+console.log(ll.get(2));
+console.log(ll.set(0, "FIRST"));
 console.log(ll);
