97c347697e8032147e781bc82b21948bad7ca182
Nim Tayo <nimaester@gmail.com>
2022-01-31 18:14:46 -0800

    doubly LinkedList recap

---
+++ recap/doublyLinkedList.js
@@ -0,0 +1,15 @@
+class Node {
+  constructor(val, next = null, prev = null) {
+    this.val = val;
+    this.next = next;
+    this.prev = prev;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
