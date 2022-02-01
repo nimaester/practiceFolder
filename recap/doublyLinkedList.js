de0474c06b21bcffd4e549e8dc07fb2f155b075e
Nim Tayo <nimaester@gmail.com>
2022-01-31 19:07:05 -0800

    recap doublyLinkedList push pop shift unshift

--- recap/doublyLinkedList.js
+++ recap/doublyLinkedList.js
@@ -12,4 +12,52 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  push(val) {
+    const node = new Node(val);
+    if (!this.length) {
+      this.head = node;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
+    }
+    this.tail = node;
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.length) return undefined;
+    let oldTail = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = oldTail.prev;
+      this.tail.next = null;
+      oldTail.prev = null;
+    }
+    this.length--;
+    return oldTail;
+  }
+  shift() {
+    if (!this.length) return undefined;
+    let oldHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    this.length--;
+    return oldHead;
+  }
 }
+
+let dll = new DoublyLinkedList();
+dll.push(1);
+dll.push(2);
+dll.push(3);
+console.log(dll.shift());
+console.log(dll.shift());
+console.log(dll);
