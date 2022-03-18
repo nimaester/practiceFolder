673fbbc6c8ac3987040d8765b6e6becec8e42d26
Nim Tayo <nimaester@gmail.com>
2022-03-18 16:19:18 -0700

    dijkstras

---
+++ recap/weightedGraph.js
@@ -0,0 +1,23 @@
+class WeightedGraph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+  addEdge(v1, v2, weight) {
+    if (this.adjacencyList[v1] && this.adjacencyList[v2]) {
+      this.adjacencyList[v1].push({ val: v2, weight });
+      this.adjacencyList[v2].push({ val: v1, weight });
+    }
+  }
+}
+
+const wg = new WeightedGraph();
+wg.addVertex("A");
+wg.addVertex("B");
+wg.addVertex("C");
+wg.addEdge("A", "B", 5);
+wg.addEdge("A", "C", 3);
+wg.addEdge("B", "C", 1);
+console.log(wg.adjacencyList);
