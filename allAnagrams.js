5aba811e1fa8895a3389145a7f03c525fa154b19
Nim Tayo <nimaester@gmail.com>
2022-06-24 14:01:37 -0700

    solved allAnagrams

--- allAnagrams.js
+++ allAnagrams.js
@@ -1,9 +1,7 @@
 const allAnagrams = (str) => {
   const anagrams = [];
   const getAnagrams = (str, result) => {
-    if (str.length === 0) {
-      anagrams.push(result);
-    }
+    if (str.length === 0) anagrams.push(result);
     for (let i = 0; i < str.length; i++) {
       let remain = str.substring(0, i) + str.substring(i + 1);
       getAnagrams(remain, result + str[i]);
