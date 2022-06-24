371698ffefeb3c8154f003aa048f11c962763c42
Nim Tayo <nimaester@gmail.com>
2022-06-24 14:32:01 -0700

    recap binary search

--- practice.js
+++ practice.js
@@ -59,3 +59,17 @@ const mergesort = (arr) => {
 };
 
 console.log(mergesort([3, 4, 1, 2, 8, 0]));
+
+const binarySearch = (arr, target) => {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    if (arr[mid] === target) return true;
+    else if (arr[mid] < target) start = mid + 1;
+    else if (arr[mid] > target) end = mid - 1;
+  }
+  return false;
+};
+
+console.log(binarySearch([1, 2, 3, 4, 5], 5));
