ed1d7be09a57c0980b2e6a7cbcdf69ff5019b286
Nim Tayo <nimaester@gmail.com>
2022-06-27 21:23:21 -0700

    binary search

--- practice.js
+++ practice.js
@@ -65,9 +65,13 @@ const binarySearch = (arr, target) => {
   let end = arr.length - 1;
   while (start <= end) {
     let mid = Math.floor((start + end) / 2);
-    if (arr[mid] === target) return true;
-    else if (arr[mid] < target) start = mid + 1;
-    else if (arr[mid] > target) end = mid - 1;
+    if (arr[mid] === target) {
+      return true;
+    } else if (arr[mid] < target) {
+      start = mid + 1;
+    } else if (arr[mid] > target) {
+      end = mid - 1;
+    }
   }
   return false;
 };
